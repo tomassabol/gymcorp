af2b225e77b36eac56e92c8871cefb79bda35dcd
tomassabol <tomassabol2@gmail.com>
2019-12-21 00:29:58 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+const navSlide = () => {
+  const hamburger = document.querySelector('#hamburger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelector('nav-links li');
+  //Toggle nav
+  hamburger.addEventListener('click',() => {
+    nav.classList.toggle('nav-active');
+  });
+  //animate Links
+  navLinks.forEach((link, index) => {
+    link.style.animate = 'navLinkFade 0.5s ease forwards ${index /7}s';
+    console.log(index / 7);
+  });
+}
